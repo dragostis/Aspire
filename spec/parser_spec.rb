393fadcb0403407a877d4262830aefd4368d5ee0
Dragos Tiselice <dragostiselice@gmail.com>
2015-09-22 21:17:25 +0300

    Split the values per type.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,81 +7,90 @@ describe Parser do
   let(:parser) { Parser.new }
 
   context 'values' do
-    let(:value_parser) { parser.value }
-
     it 'parses booleans' do
-      expect(value_parser).to parse('true')
-      expect(value_parser).to parse('false')
+      expect(parser.boolean).to parse 'true'
+      expect(parser.boolean).to parse 'false'
     end
 
     it 'parses integers' do
-      expect(value_parser).to parse('0')
-      expect(value_parser).to parse('10')
-      expect(value_parser).to parse('1234567890')
-      expect(value_parser).to parse('-10')
-      expect(value_parser).to parse('+10')
-      expect(value_parser).to_not parse('010')
+      expect(parser.integer).to parse '0'
+      expect(parser.integer).to parse '10'
+      expect(parser.integer).to parse '1234567890'
+      expect(parser.integer).to parse '-10'
+      expect(parser.integer).to parse '+10'
+      expect(parser.integer).to_not parse '010'
     end
 
     it 'parses floats' do
-      expect(value_parser).to parse('0.')
-      expect(value_parser).to parse('.0')
-      expect(value_parser).to parse('1234567890.1234567890')
-      expect(value_parser).to parse('-0.0')
-      expect(value_parser).to parse('+0.0')
-      expect(value_parser).to parse('0.0e+10')
-      expect(value_parser).to parse('0.0e-10')
-      expect(value_parser).to parse('-0.0E-10')
-      expect(value_parser).to_not parse('00.0')
-      expect(value_parser).to_not parse('0.0e-00')
+      expect(parser.float).to parse '0.'
+      expect(parser.float).to parse '.0'
+      expect(parser.float).to parse '1234567890.1234567890'
+      expect(parser.float).to parse '-0.0'
+      expect(parser.float).to parse '+0.0'
+      expect(parser.float).to parse '0.0e+10'
+      expect(parser.float).to parse '0.0e-10'
+      expect(parser.float).to parse '-0.0E-10'
+      expect(parser.float).to_not parse '00.0'
+      expect(parser.float).to_not parse '0.0e-00'
     end
 
     it 'parses colors' do
-      expect(value_parser).to parse('#000')
-      expect(value_parser).to parse('#000000')
-      expect(value_parser).to parse('#fff')
-      expect(value_parser).to parse('#FFFFFF')
-      expect(value_parser).to parse('#FFFFFF00')
-      expect(value_parser).to_not parse('#ggg')
-      expect(value_parser).to_not parse('#ffff')
-      expect(value_parser).to_not parse('#fffff')
-      expect(value_parser).to_not parse('#fffffff')
+      expect(parser.color).to parse '#000'
+      expect(parser.color).to parse '#000000'
+      expect(parser.color).to parse '#fff'
+      expect(parser.color).to parse '#FFFFFF'
+      expect(parser.color).to parse '#FFFFFF00'
+      expect(parser.color).to_not parse '#ggg'
+      expect(parser.color).to_not parse '#ffff'
+      expect(parser.color).to_not parse '#fffff'
+      expect(parser.color).to_not parse '#fffffff'
     end
 
     it 'parses vectors' do
-      expect(value_parser).to parse('(1,2)')
-      expect(value_parser).to parse('(1, 2, 3, 4)')
-      expect(value_parser).to parse("(\n1 \n, \n2 , \n3\n)")
-      expect(value_parser).to parse('(true, false)')
-      expect(value_parser).to parse('(0.1, 1.0)')
-      expect(value_parser).to parse('(hey, ya)')
-      expect(value_parser).to_not parse('(1)')
-      expect(value_parser).to_not parse('(#fff)')
+      expect(parser.vector).to parse '(1,2)'
+      expect(parser.vector).to parse '(1, 2, 3, 4)'
+      expect(parser.vector).to parse "(\n1 \n, \n2 , \n3\n)"
+      expect(parser.vector).to parse '(true, false)'
+      expect(parser.vector).to parse '(0.1, 1.0)'
+      expect(parser.vector).to parse '(hey, ya)'
+      expect(parser.vector).to_not parse '(1)'
+      expect(parser.vector).to_not parse '(#fff)'
     end
 
     it 'parses matrices' do
-      expect(value_parser).to parse('((1, 2), (3, 4))')
-      expect(value_parser).to parse('((1, 2), (3, 4), (1, 2), (3, 4))')
-      expect(value_parser).to parse("((\n1 \n, \n2 , \n3\n)\n, \n(4, 5))")
-      expect(value_parser).to_not parse('((1, 2))')
+      expect(parser.matrix).to parse '((1, 2), (3, 4))'
+      expect(parser.matrix).to parse '((1, 2), (3, 4), (1, 2), (3, 4))'
+      expect(parser.matrix).to parse "((\n1 \n, \n2 , \n3\n)\n, \n(4, 5))"
+      expect(parser.matrix).to_not parse '((1, 2))'
     end
 
     it 'parses arrays' do
-      expect(value_parser).to parse('[]')
-      expect(value_parser).to parse('[1]')
-      expect(value_parser).to parse('[1, 2, 3, 4, 5]')
-      expect(value_parser).to parse("[\n1 \n, \n2 , \n3\n]")
-      expect(value_parser).to parse('[((1, 2), (3, 4))]')
-      expect(value_parser).to parse('[[]]')
+      expect(parser.array).to parse '[]'
+      expect(parser.array).to parse '[1]'
+      expect(parser.array).to parse '[1, 2, 3, 4, 5]'
+      expect(parser.array).to parse "[\n1 \n, \n2 , \n3\n]"
+      expect(parser.array).to parse '[((1, 2), (3, 4))]'
+      expect(parser.array).to parse '[[]]'
     end
 
     it 'parses identifiers' do
-      expect(value_parser).to parse('camelCase')
-      expect(value_parser).to parse('snake_case')
-      expect(value_parser).to parse('numb3rs')
-      expect(value_parser).to parse('ev3ry_th1ng')
-      expect(value_parser).to_not parse('')
-      expect(value_parser).to_not parse('3n')
+      expect(parser.identifier).to parse 'camelCase'
+      expect(parser.identifier).to parse 'snake_case'
+      expect(parser.identifier).to parse 'numb3rs'
+      expect(parser.identifier).to parse 'ev3ry_th1ng'
+      expect(parser.identifier).to_not parse ''
+      expect(parser.identifier).to_not parse '3n'
+    end
+
+    it 'parses values' do
+      expect(parser.value).to parse 'true'
+      expect(parser.value).to parse '123'
+      expect(parser.value).to parse '-0.0'
+      expect(parser.value).to parse '#fff'
+      expect(parser.value).to parse '(0, 0)'
+      expect(parser.value).to parse '((0, 0), (0, 0))'
+      expect(parser.value).to parse '[1]'
+      expect(parser.value).to parse 'id3n_tifier'
     end
   end
 
